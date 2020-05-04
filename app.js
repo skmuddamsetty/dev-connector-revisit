0e96334e6cc7b6504312608f7589d2093e1b354a
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-04 00:28:03 -0400

    added morgan

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const morgan = require('morgan');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 const userRouter = require('./routes/user');
 
 /*****************Global Middlwares Start************************/
-/**
- * this is bodyParser middleware
- * data from body is added to the request object with this step
- * using the limit limts the data sent by the user to be less than or equal to 10 KB
- */
+/* 
+    Development Logging 
+    Middleware to log the type of request to console
+*/
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+/*
+    this is bodyParser middleware
+    data from body is added to the request object with this step
+    using the limit limts the data sent by the user to be less than or equal to 10 KB
+*/
 app.use(express.json({ limit: '10kb' }));
 /*****************Helper Methods End************************/
 
