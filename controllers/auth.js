c9f53e48e6722d495f5febe5f532443c990cb67d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-04 23:19:28 -0400

    added protect and auth routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const { promisify } = require('util');
 
 /**
  * @route  POST /api/v1/users/signup
@@ -12,6 +14,58 @@ exports.signup = catchAsync(async (req, res) => {
   createAndSendToken(newUser, 201, res);
 });
 
+/**
+ * @route  GET /api/v1/users/auth
+ * @desc   Finds and returns the user with the id based on the id coming from protect *         middleware
+ * @access Public
+ */
+exports.auth = catchAsync(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  res.status(200).json({ status: 'success', user });
+});
+
+/**
+ * checks if the user is logged in and is a valis user
+ */
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) get the token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies && req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+  // 2) validate token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) check if the user still exists
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(
+      new AppError('The user belonging to this token does not exist', 401)
+    );
+  }
+  // 4) check if user changed the password after issuing the JWT
+  if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError(
+        'User recently has changed the password. Please log in again',
+        401
+      )
+    );
+  }
+  // 5) GRANT ACCESS TO THE PROTECTED ROUTE
+  req.user = freshUser;
+  next();
+});
 /*****************Helper Methods Start************************/
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,5 @@ const authController = require('../controllers/auth');
 
 const router = express.Router();
 router.post('/signup', authController.signup);
+router.get('/auth', authController.protect, authController.auth);
 module.exports = router;
