310302444ef05855f4ebc5e5327bfef7ea1dfff1
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-06 07:37:11 -0400

    created posts model

---
+++ models/Post.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const postSchema = new mongoose.Schema({
+  // parent referencing user and profile documents
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Post must belong to a user.'],
+  },
+  text: {
+    type: String,
+    required: [true, 'Please enter valid text!'],
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+      text: {
+        type: String,
+        required: [true, 'Please provide valid text for a comment!'],
+      },
+      avatar: {
+        type: String,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
