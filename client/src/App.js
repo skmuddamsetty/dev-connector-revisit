eeab3d0e74999dd8644596ed1ae8a0cd805e13a4
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-07 09:06:14 -0400

    Init Commit with redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,25 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store/configureStore';
 
 const App = () => (
-  <BrowserRouter>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-        </Switch>
-      </section>
-    </Fragment>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </Switch>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
