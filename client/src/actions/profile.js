a0f4ab4550f378ba90ee15338296cd035b366872
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-25 17:23:20 -0400

    48. Create Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-// import { setAlert } from './alerts';
+import { setAlert } from './alerts';
 import { PROFILE_ERROR, GET_PROFILE } from './types';
 
 // Get Current User Profile
@@ -10,6 +10,40 @@ export const getCurrentUserProfile = () => async (dispatch) => {
     dispatch({ type: GET_PROFILE, payload: res.data.profile });
   } catch (error) {
     console.log(error.response.data.message);
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        message: error.response.data.message,
+      },
+    });
+  }
+};
+
+// Create or Update Profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        'content-type': 'application/json',
+      },
+    };
+    const res = await axios.post('/api/v1/profile', formData, config);
+    dispatch({ type: GET_PROFILE, payload: res.data.profile });
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (error) {
+    console.log(error.response.data.message);
+    // TODO
+    // const erroObj = err.response.data;
+    // if (erroObj && erroObj.error && erroObj.error.message) {
+    //   dispatch(setAlert(erroObj.error.message, 'danger'));
+    // } else {
+    //   dispatch(setAlert(err.response.data, 'danger'));
+    // }
     dispatch({
       type: PROFILE_ERROR,
       payload: {
--- client/src/components/profile-form/CreateProfile.js
+++ client/src/components/profile-form/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
+import { withRouter } from 'react-router-dom';
 
-const CreateProfile = (props) => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -38,6 +40,10 @@ const CreateProfile = (props) => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
   return (
     <Fragment>
       <h1 className='large text-primary'>Create Your Profile</h1>
@@ -46,7 +52,7 @@ const CreateProfile = (props) => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className='form'>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
         <div className='form-group'>
           <select name='status' value={status} onChange={(e) => onChange(e)}>
             <option value='0'>* Select Professional Status</option>
@@ -214,4 +220,11 @@ const CreateProfile = (props) => {
 
 CreateProfile.propTypes = {};
 
-export default connect()(CreateProfile);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createProfile: (formData, history) =>
+      dispatch(createProfile(formData, history)),
+  };
+};
+// withRouter is needed because we are extracting the history object in the component
+export default connect(null, mapDispatchToProps)(withRouter(CreateProfile));
